684cce31cd37b46c388445988139a63e590d8f52
lynjung <snowinaugust4@gmail.com>
2026-01-04 17:57:04 -0500

    Time: 16 ms (45.64%) | Memory: 98 MB (7.31%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i])) {
+                return true;
+            }
+            else {
+                hash.put(nums[i], i);
+            }
+        }
+
+        return false;
+    }
+}
