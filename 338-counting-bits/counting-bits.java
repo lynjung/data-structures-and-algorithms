5e6f979ea5f57e4f09ca4c2c95a3987703c2d0a7
lynjung <snowinaugust4@gmail.com>
2026-01-15 22:06:46 -0500

    Time: 2 ms (96.48%) | Memory: 48.8 MB (37.08%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        int offset = 1;
+
+        for (int i = 1; i < n + 1; i++) {
+            if (i == (2 * offset)) {
+                offset = i;
+            }
+            ans[i] = 1 + ans[i - offset];
+        }
+        return ans;
+    }
+}
