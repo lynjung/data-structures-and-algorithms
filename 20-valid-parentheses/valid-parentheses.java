683dea8f6f67a24efd046c913303f9caf785cde6
lynjung <snowinaugust4@gmail.com>
2026-02-18 18:33:06 -0500

    Time: 5 ms (16.91%) | Memory: 43.6 MB (26.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        
         HashMap<Character, Character> hashmap = new HashMap<>();
 
         hashmap.put('(', ')');
         hashmap.put('{', '}');
         hashmap.put('[', ']');
 
+        Stack<Character> stack = new Stack<>();
+
         for (char c : s.toCharArray()) {
             if (hashmap.containsKey(c)) {
                 stack.push(c);
             }
-
             else {
-                if (stack.empty()) {
-                    return false;
-                }
-                char recent = stack.pop();
-                if (hashmap.get(recent) != c) {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (hashmap.get(top) != c) {
                     return false;
                 }
             }
         }
-        return stack.empty();
+        return stack.isEmpty();
     }
 }
