616c928630404a9afacffa4b06e282dc081f9918
Lyn Jung <snowinaugust4@gmail.com>
2026-05-12 15:11:07 -0400

    Time: 0 ms (100.00%) | Memory: 48.2 MB (73.05%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,14 +1,18 @@
 class Solution {
     public void reverseString(char[] s) {
         int left = 0;
-        int right = s.length - 1;
+        int right = s.length - 1; 
 
         while (left < right) {
-            char dum = s[left];
-            s[left] = s[right];
-            s[right] = dum;
+            char letter1 = s[left];
+            char letter2 = s[right];
+
+            s[left] = letter2;
+            s[right] = letter1;
+
             left++;
             right--;
         }
+        return;
     }
 }
