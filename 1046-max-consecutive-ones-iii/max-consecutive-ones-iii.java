fcaf10b78f9d758a6a6e83cb1193a92a8b64339d
Lyn Jung <snowinaugust4@gmail.com>
2026-05-12 22:38:20 -0400

    Time: 3 ms (97.12%) | Memory: 52.2 MB (54.15%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,23 +1,23 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        
+        // basically find the max length subarray that has less than or equal to k 0's
+
         int left = 0;
-        int count = 0;
-        int max = 0;
+        int zeros = 0;
+        int length = 0;
 
         for (int right = 0; right < nums.length; right++) {
             if (nums[right] == 0) {
-                count++;
+                zeros++;
             }
-            while (count > k) {
+            while (zeros > k) {
                 if (nums[left] == 0) {
-                    count--;
+                    zeros--;
                 }
                 left++;
             }
-        
-            max = Math.max(max, right - left + 1);
+            length = Math.max(length, right - left + 1);
         }
-        return max;
+        return length;
     }
 }
