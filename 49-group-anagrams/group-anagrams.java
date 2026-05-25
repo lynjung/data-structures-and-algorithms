b30d4b2203c0176e1ab74d8d1dba0c3b242a52ae
Lyn Jung <snowinaugust4@gmail.com>
2026-05-25 02:25:46 -0400

    Time: 8 ms (38.83%) | Memory: 49.4 MB (83.69%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] arr = word.toCharArray();
+            Arrays.sort(arr);
+            String sorted = new String(arr);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+            
+            map.get(sorted).add(word);
+        }
+        return new ArrayList(map.values());
+    }
+}
