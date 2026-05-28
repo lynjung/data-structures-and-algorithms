744a794a0ba0339c45baa362f28c1c5dae74ad84
Lyn Jung <snowinaugust4@gmail.com>
2026-05-28 19:21:29 -0400

    Time: 0 ms (100.00%) | Memory: 45.7 MB (15.43%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -10,15 +10,12 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode curr = head;
+       ListNode curr = head;
 
         while (curr != null && curr.next != null) {
             if (curr.val == curr.next.val) {
                 curr.next = curr.next.next;
-            }
-            else {
-                curr = curr.next;
-            }
+            } else {curr = curr.next;}
         }
         return head;
     }
