95939c7e842c6ea0c79f571a84860e3b9e44ad44
Lyn Jung <snowinaugust4@gmail.com>
2026-06-07 14:01:10 -0400

    Time: 60 ms (78.58%) | Memory: 107.5 MB (46.82%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>(); // store the indices
+        int[] ans = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int j = stack.pop();
+                ans[j] = i - j;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
