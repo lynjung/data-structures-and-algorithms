9447cc57863a37300e7daadafe52ab85e0e82d14
Lyn Jung <snowinaugust4@gmail.com>
2026-06-07 15:51:25 -0400

    Time: 4 ms (75.40%) | Memory: 45.2 MB (49.77%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+
+        int[] ans = new int[nums1.length];
+        for (int j = 0; j < nums1.length; j++) {
+            ans[j] = map.getOrDefault(nums1[j], -1);
+        }
+        return ans;
+    }
+}
