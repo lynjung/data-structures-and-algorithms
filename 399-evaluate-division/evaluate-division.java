1268a137e6fc1e9fd7a25230cc23498e0d5df159
Lyn Jung <snowinaugust4@gmail.com>
2026-07-04 16:24:12 -0400

    Time: 2 ms (30.04%) | Memory: 47 MB (48.37%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.java
@@ -0,0 +1,71 @@
+class Pair {
+    String node;
+    double ratio;
+
+    Pair(String node, double ratio) {
+        this.node = node;
+        this.ratio = ratio;
+    }
+}
+
+class Solution {
+    Map<String, Map<String, Double>> graph = new HashMap<>();
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        for (int i = 0; i < equations.size(); i++) {
+            String numerator = equations.get(i).get(0);
+            String denominator = equations.get(i).get(1);
+            double value = values[i];
+
+            if (!graph.containsKey(numerator)) {
+                graph.put(numerator, new HashMap<>());
+            }
+            if (!graph.containsKey(denominator)) {
+                graph.put(denominator, new HashMap<>());
+            }
+
+            graph.get(numerator).put(denominator, value);
+            graph.get(denominator).put(numerator, 1 / value);
+        }
+
+        double[] ans = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            ans[i] = answerQuery(queries.get(i).get(0), queries.get(i).get(1));
+        }
+
+        return ans;
+    }
+
+    public double answerQuery(String start, String end) {
+        if (!graph.containsKey(start)) {
+            return -1;
+        }
+
+        Set<String> seen = new HashSet<>();
+        Stack<Pair> stack = new Stack<>();
+
+        seen.add(start);
+        stack.push(new Pair(start, 1));
+
+        while (!stack.isEmpty()) {
+            Pair pair = stack.pop();
+            String node = pair.node;
+            double ratio = pair.ratio;
+
+            if (node.equals(end)) {
+                return ratio;
+            }
+
+            if (graph.containsKey(node)) {
+                for (String neighbor: graph.get(node).keySet()) {
+                    if (!seen.contains(neighbor)) {
+                        seen.add(neighbor);
+                        stack.push(new Pair(neighbor, ratio * graph.get(node).get(neighbor)));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
