dcbbe0cc37c06fd6d58e923f939832f3f8b2ba70
Lyn Jung <snowinaugust4@gmail.com>
2026-07-22 13:38:28 -0400

    Time: 185 ms (5.29%) | Memory: 99.3 MB (80.31%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,36 @@
+class Solution {
+    double limit;
+
+    public int minSpeedOnTime(int[] dist, double hour) {
+        if (dist.length > Math.ceil(hour)) {
+            return -1;
+        }
+
+        limit = hour;
+        int left = 1;
+        int right = (int) Math.pow(10, 7);
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // speed
+
+            if (check(mid, dist)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean check(int s, int[] dist) {
+        double t = 0;
+
+        for (double d : dist) {
+            t = Math.ceil(t);
+            t += d / s;
+        }
+
+        return t <= limit;
+    }
+}
