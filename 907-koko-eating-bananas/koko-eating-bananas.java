2770a6bb0a5c4455cf2cb77390f6fb4265dbcc22
Lyn Jung <snowinaugust4@gmail.com>
2026-07-21 22:01:09 -0400

    Time: 25 ms (21.80%) | Memory: 47.6 MB (87.63%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    int limit;
+
+    public int minEatingSpeed(int[] piles, int h) {
+        limit = h;
+        int left = 1;
+        int right = 0;
+
+        for (int bananas : piles) {
+            right = Math.max(right, bananas);
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (valid(mid, piles)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean valid(double k, int[] piles) {
+        int hours = 0;
+        for (double bananas : piles) {
+            hours += Math.ceil(bananas / k);
+        }
+
+        return hours <= limit;
+    }
+}
