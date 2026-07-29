d00f0f14687a122fbcca199a019e1a6b0a1f6e8a
Lyn Jung <snowinaugust4@gmail.com>
2026-07-29 18:54:50 -0400

    Time: 4 ms (14.08%) | Memory: 43.2 MB (6.70%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int size;
+
+    public int totalNQueens(int n) {
+        size = n;
+        return backtrack(0, new HashSet<>(), new HashSet<>(), new HashSet<>());
+    }
+
+    private int backtrack(int row, Set<Integer> diagonals, Set<Integer> antiDiagonals, Set<Integer> cols) {
+        if (row == size) {
+            return 1;
+        }
+
+        int solutions = 0;
+
+        for (int col = 0; col < size; col++) {
+            int currDiagonal = row - col;
+            int currAntiDiagonal = row + col;
+
+            if (cols.contains(col) || diagonals.contains(currDiagonal) || antiDiagonals.contains(currAntiDiagonal)) {
+                continue;
+            }
+
+            cols.add(col);
+            diagonals.add(currDiagonal);
+            antiDiagonals.add(currAntiDiagonal);
+
+            solutions += backtrack(row + 1, diagonals, antiDiagonals, cols);
+
+            cols.remove(col);
+            diagonals.remove(currDiagonal);
+            antiDiagonals.remove(currAntiDiagonal);
+        }
+
+        return solutions;
+    }
+}
