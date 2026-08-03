2f773987d1c766eb072932747970a721f10f1ed2
Lyn Jung <snowinaugust4@gmail.com>
2026-08-02 20:33:03 -0400

    Time: 378 ms (11.82%) | Memory: 47 MB (24.26%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int lengthOfLIS(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = Math.max(ans, dp(i, nums));
+        }
+
+        return ans;
+    }
+
+    private int dp(int i, int[] nums) {
+        if (memo.containsKey(i)) {
+            return memo.get(i);
+        }
+
+        int ans = 1;
+
+        for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                ans = Math.max(ans, dp(j, nums) + 1);
+            }
+        }
+
+        memo.put(i, ans);
+        return ans;
+    }
+}
