acf2b6179593229101e4476a8b173f542013537c
Lyn Jung <snowinaugust4@gmail.com>
2026-08-20 14:28:24 -0400

    Time: 0 ms (100.00%) | Memory: 43 MB (5.76%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,17 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-
     public int rob(int[] nums) {
-        return dp(nums.length - 1, nums);
-    }
-
-    public int dp(int i, int[] nums) {
-        if (i == 0) {
-            return nums[i];
-        }
-        if (i == 1) {
-            return Math.max(nums[0], nums[1]);
+        if (nums.length == 1) {
+            return nums[0];
         }
 
-        if (memo.containsKey(i)) {
-            return memo.get(i);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
 
-        memo.put(i, Math.max(dp(i - 2, nums) + nums[i], dp(i - 1, nums)));
-        return memo.get(i);
+        return dp[nums.length - 1];
     }
 }
