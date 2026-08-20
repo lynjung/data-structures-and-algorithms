ef11a094d8275dc3d92b8f95f80a2954f983d741
Lyn Jung <snowinaugust4@gmail.com>
2026-08-20 14:43:16 -0400

    Time: 0 ms (100.00%) | Memory: 43.1 MB (5.76%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -4,14 +4,15 @@ class Solution {
             return nums[0];
         }
 
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int backTwo = nums[0];
+        int backOne = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            int temp = backOne;
+            backOne = Math.max(backTwo + nums[i], backOne);
+            backTwo = temp;
         }
 
-        return dp[nums.length - 1];
+        return backOne;
     }
 }
