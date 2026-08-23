b70b6b3c73ef017b544fbdba85d6b3d899a3cf86
Lyn Jung <snowinaugust4@gmail.com>
2026-08-23 00:01:57 -0400

    Time: 37 ms (5.08%) | Memory: 194.5 MB (5.08%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -1,19 +1,19 @@
 class Solution {
     Map<Integer, Long> memo = new HashMap<>();
-    
+
     public long mostPoints(int[][] questions) {
         return dp(0, questions);
     }
-    
+
     public long dp(int i, int[][] questions) {
         if (i >= questions.length) {
             return 0;
         }
-        
-        if (memo.containsKey(i)) {
+
+        if (memo.containsKey(i)) {    
             return memo.get(i);
         }
-        
+
         int j = i + questions[i][1] + 1;
         memo.put(i, Math.max(questions[i][0] + dp(j, questions), dp(i + 1, questions)));
         return memo.get(i);
