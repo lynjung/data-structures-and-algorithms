ebe37e9a64cfdd7ee7a78a1e34ad2175c7684521
Lyn Jung <snowinaugust4@gmail.com>
2026-09-17 23:37:41 -0400

    Time: 2 ms (99.39%) | Memory: 46.6 MB (95.79%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -4,11 +4,9 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int comp = target - nums[i];
-
             if (map.containsKey(comp)) {
                 return new int[]{map.get(comp), i};
             }
-
             map.put(nums[i], i);
         }
         return null;
